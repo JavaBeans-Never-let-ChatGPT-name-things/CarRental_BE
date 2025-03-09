28c094d066e044fca0d72bdc3a05bdcfcb50d11a
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-03-09 21:44:55 +0700

    Create Account Entity and its related enums

---
+++ backend/src/main/java/com/example/backend/entity/AccountEntity.java
@@ -0,0 +1,56 @@
+package com.example.backend.entity;
+
+import com.example.backend.entity.enums.AccountRole;
+import com.example.backend.entity.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "accounts",
+    indexes = {
+        @Index(
+            columnList = "username",
+            unique = true
+        )
+    })
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountEntity extends AbstractAuditing<Long>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "username", unique = true, nullable = false, updatable = false)
+    String username;
+
+    @Column(name = "password_hash", nullable = false)
+    String passwordHash;
+
+    @Column(name = "email", unique = true, nullable = false)
+    String email;
+
+    @Column(name = "display_name", nullable = false)
+    String displayName;
+
+    @Column(name = "address")
+    String address;
+
+    @Column(name = "phone_number")
+    String phoneNumber;
+
+    @Column(name = "avatar", columnDefinition = "text")
+    String avatarUrl;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    AccountStatus accountStatus = AccountStatus.ACTIVE;
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    AccountRole accountRole = AccountRole.USER;
+}
---
+++ backend/src/main/java/com/example/backend/entity/enums/AccountStatus.java
@@ -0,0 +1,5 @@
+package com.example.backend.entity.enums;
+
+public enum AccountStatus {
+    ACTIVE, BANNED
+}
