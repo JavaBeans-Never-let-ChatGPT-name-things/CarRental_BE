e8099d1c84f9dc491c38d95e9dd4c88afc7409af
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-05-09 23:32:19 +0700

    Add confirmPickup, reportLost, extend and confirmReturn Contract in Contract Service Layer

--- backend/src/main/java/com/example/backend/service/ContractService.java
+++ backend/src/main/java/com/example/backend/service/ContractService.java
@@ -1,6 +1,12 @@
 package com.example.backend.service;
 
+import com.example.backend.entity.enums.ReturnCarStatus;
+
 public interface ContractService {
     String retryContractSuccess(Long contractId, String token);
     String retryContract(Long contractId, String token);
+    String comfirmPickupContract(Long contractId, String token);
+    String reportLostContract(Long contractId);
+    String extendContract(Long contractId, String token, int extraDays);
+    String confirmReturnContract(Long contractId, String token, ReturnCarStatus returnCarStatus);
 }
--- backend/src/main/java/com/example/backend/service/ContractServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/ContractServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.backend.entity.enums.CarState;
 import com.example.backend.entity.enums.ContractStatus;
 import com.example.backend.entity.enums.PaymentStatus;
 import com.example.backend.entity.enums.ReturnCarStatus;
+import com.example.backend.repository.AccountRepository;
 import com.example.backend.repository.ContractRepository;
 import com.example.backend.repository.FCMRepository;
 import com.example.backend.repository.NotificationRepository;
@@ -14,7 +15,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -32,11 +32,11 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @Slf4j
-@Order(100)
 public class ContractServiceImpl implements ContractService{
     ContractRepository contractRepository;
     JwtService jwtService;
     FCMRepository fcmRepository;
+    AccountRepository accountRepository;
     FirebaseMessagingService firebaseMessagingService;
     PayOsService payOsService;
     Map<Long, String> pendingPayments = new ConcurrentHashMap<>();
@@ -105,6 +105,90 @@ public class ContractServiceImpl implements ContractService{
         return "Successfully holded for contract id: " + rentalContractEntity.getId();
     }
 
+    @Override
+    public String comfirmPickupContract(Long contractId, String token) {
+        String username = jwtService.extractUserName(token);
+        AccountEntity accountEntity = accountRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Account not found"));
+        RentalContractEntity rentalContract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new RuntimeException("Contract not found"));
+        if (rentalContract.getPaymentStatus() != PaymentStatus.SUCCESS)
+        {
+            return "Payment is not successful";
+        }
+        if (rentalContract.getStartDate().isAfter(LocalDate.now()))
+        {
+            return "Contract is not started yet";
+        }
+        rentalContract.setEmployee(accountEntity);
+        rentalContract.setContractStatus(ContractStatus.PICKED_UP);
+        contractRepository.save(rentalContract);
+        return "";
+    }
+
+    @Override
+    public String reportLostContract(Long contractId) {
+        RentalContractEntity rentalContract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new RuntimeException("Contract not found"));
+        rentalContract.setContractStatus(ContractStatus.COMPLETE);
+        rentalContract.setReturnCarStatus(ReturnCarStatus.LOST);
+        rentalContract.setPenaltyFee(rentalContract.getPenaltyFee()+rentalContract.getCar().getRentalPrice() * 0.8f);
+        contractRepository.save(rentalContract);
+        sendNotificationToEmployee(rentalContract, rentalContract.getCar().getCarImageUrl(), rentalContract.getEmployee(), rentalContract.getPenaltyFee());
+        return "";
+    }
+
+    @Override
+    public String extendContract(Long contractId, String token, int extraDays) {
+        RentalContractEntity rentalContract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new RuntimeException("Contract not found"));
+        if (rentalContract.getEndDate().minusDays(1).isAfter(LocalDate.now()))
+            throw new IllegalStateException("Too early to extend");
+        if (extraDays > 4 || !rentalContract.isExtendable())
+            throw new IllegalArgumentException("Cannot extend");
+
+        rentalContract.setEndDate(rentalContract.getEndDate().plusDays(extraDays));
+        rentalContract.setExtendable(false);
+        contractRepository.save(rentalContract);
+        return "Successfully extended contract id: " + rentalContract.getId()+ " by " + extraDays + " days";
+    }
+
+    @Override
+    public String confirmReturnContract(Long contractId, String token, ReturnCarStatus returnCarStatus) {
+        RentalContractEntity rentalContract = contractRepository.findById(contractId)
+                .orElseThrow(() -> new RuntimeException("Contract not found"));
+        rentalContract.setReturnDate(LocalDate.now());
+        rentalContract.setReturnCarStatus(returnCarStatus);
+        if (returnCarStatus.equals(ReturnCarStatus.DAMAGED))
+        {
+            rentalContract.setPenaltyFee(rentalContract.getPenaltyFee() + 100);
+        }
+        rentalContract.setContractStatus(ContractStatus.COMPLETE);
+        CarEntity carEntity = rentalContract.getCar();
+        carEntity.setState(CarState.AVAILABLE);
+        contractRepository.save(rentalContract);
+        return "Successfully returned contract id: " + rentalContract.getId();
+    }
+
+    private void sendNotificationToEmployee(RentalContractEntity rentalContract, String carImageUrl, AccountEntity employee, float penaltyFee) {
+        List<FCMTokenEntity> fcmTokenList = fcmRepository.findAllByUserId(employee.getId());
+        if (fcmTokenList.isEmpty()) {
+            log.info("No tokens found for employee");
+            return;
+        }
+        Long id = rentalContract.getId();
+        NotificationFCMRequest notificationFCMRequest = new NotificationFCMRequest();
+        String username = rentalContract.getAccount().getUsername();
+        notificationFCMRequest.setTitle("User " + username + " reported Car " + rentalContract.getCar().getId() + " as lost");
+        notificationFCMRequest.setBody("Contract No: " + id + " of user " + username + " is reported as lost. Penalty fee: " + penaltyFee);
+        notificationFCMRequest.setImage(carImageUrl);
+        for (FCMTokenEntity fcmTokenEntity : fcmTokenList) {
+            String token = fcmTokenEntity.getToken();
+            notificationFCMRequest.setToken(token);
+            firebaseMessagingService.sendNotification(notificationFCMRequest);
+        }
+    }
+
     @Scheduled(cron = "0 0 0 * * ?")
     @Transactional
     public void updateContractsStatus() {
@@ -114,6 +198,14 @@ public class ContractServiceImpl implements ContractService{
         for (RentalContractEntity contract: contracts) {
             log.info("Checking contract ID {} with status {}", contract.getId(), contract.getContractStatus());
             log.info("Start date: {}, End date: {}, Return date: {}", contract.getStartDate(), contract.getEndDate(), contract.getReturnDate());
+            if (contract.getContractStatus().equals(ContractStatus.BOOKED)
+                    && contract.getStartDate().isEqual(currentDate)
+                    && contract.getPaymentStatus().equals(PaymentStatus.SUCCESS)){
+                contract.setContractStatus(ContractStatus.EXPIRED);
+                contract.getCar().setState(CarState.AVAILABLE);
+                contractRepository.save(contract);
+                sendNotification(contract.getId(), contract.getCar().getCarImageUrl(), contract.getAccount(), ContractStatus.BOOKED, 0);
+            }
             if (contract.getContractStatus().equals(ContractStatus.BOOKED)
              && contract.getStartDate().isBefore(currentDate)
              && contract.getPaymentStatus().equals(PaymentStatus.SUCCESS)){
@@ -138,6 +230,16 @@ public class ContractServiceImpl implements ContractService{
                 contractRepository.save(contract);
                 log.info("Contract {} is still overdue", contract.getId());
                 sendNotification(contract.getId(), contract.getCar().getCarImageUrl(), contract.getAccount(), ContractStatus.OVERDUE, contract.getPenaltyFee());
+
+                if (contract.getEndDate().plusDays(3).isBefore(currentDate) && contract.getEmployee() != null) {
+                    sendNotificationToEmployee(
+                            contract,
+                            contract.getCar().getCarImageUrl(),
+                            contract.getEmployee(),
+                            contract.getPenaltyFee()
+                    );
+                    log.info("Notified employee about long overdue contract {}", contract.getId());
+                }
             }
         }
     }
@@ -150,6 +252,10 @@ public class ContractServiceImpl implements ContractService{
         }
         NotificationFCMRequest notificationFCMRequest = new NotificationFCMRequest();
         notificationFCMRequest.setTitle("Contract No: " + contractId + " status changed to " + contractStatus.name());
+        if (contractStatus.equals(ContractStatus.BOOKED)){
+            notificationFCMRequest.setTitle("Contrct No: " + contractId + " started today");
+            notificationFCMRequest.setBody("Please remember to pick up the car today");
+        }
         if (contractStatus.equals(ContractStatus.EXPIRED)){
             notificationFCMRequest.setBody("Your contract has expired");
         }
