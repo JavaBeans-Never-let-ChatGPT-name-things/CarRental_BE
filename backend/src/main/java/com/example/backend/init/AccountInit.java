af9d5eb12b294a91d9cd140b0b3ed4282c1d518e
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-05-13 23:19:17 +0700

    Init more user

--- backend/src/main/java/com/example/backend/init/AccountInit.java
+++ backend/src/main/java/com/example/backend/init/AccountInit.java
@@ -12,6 +12,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @Order(1)
 @RequiredArgsConstructor
@@ -19,50 +22,75 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AccountInit implements CommandLineRunner {
     AccountRepository accountRepository;
+
     @Override
     public void run(String... args) throws Exception {
-        if (accountRepository.count() > 0)
-        {
+        if (accountRepository.count() > 0) {
             log.info("Accounts already exist");
+            return;
         }
-        else {
-            log.info("No accounts found, creating new account");
-            accountRepository.save(
-                    AccountEntity.builder()
-                            .username("test")
-                            .passwordHash(new BCryptPasswordEncoder(12).encode("123456"))
-                            .accountRole(AccountRole.USER)
-                            .gender(1)
-                            .enabled(true)
-                            .email("user@gmail.com")
-                            .displayName("Test User")
-                            .build()
-            );
-            log.info("User account created successfully");
 
-            accountRepository.save(
-                    AccountEntity.builder()
-                            .username("employee")
-                            .passwordHash(new BCryptPasswordEncoder(12).encode("123456"))
-                            .gender(1)
-                            .accountRole(AccountRole.EMPLOYEE)
-                            .enabled(true)
-                            .email("employee@gmail.com")
-                            .displayName("Employee")
-                            .build());
-            log.info("Employee account created successfully");
+        log.info("No accounts found, creating new accounts");
 
-            accountRepository.save(
-                    AccountEntity.builder()
-                            .username("admin")
-                            .passwordHash(new BCryptPasswordEncoder(12).encode("123456"))
-                            .gender(1)
-                            .accountRole(AccountRole.ADMIN)
-                            .enabled(true)
-                            .email("admin@gmail.com")
-                            .displayName("Admin")
-                            .build());
-            log.info("Admin account created successfully");
+        List<AccountEntity> accounts = new ArrayList<>();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+        // Default accounts
+        accounts.add(AccountEntity.builder()
+                .username("test")
+                .passwordHash(encoder.encode("123456"))
+                .accountRole(AccountRole.USER)
+                .gender(1)
+                .enabled(true)
+                .email("user@gmail.com")
+                .displayName("Test User")
+                .build());
+
+        accounts.add(AccountEntity.builder()
+                .username("employee")
+                .passwordHash(encoder.encode("123456"))
+                .gender(1)
+                .accountRole(AccountRole.EMPLOYEE)
+                .enabled(true)
+                .email("employee@gmail.com")
+                .displayName("Employee")
+                .build());
+
+        accounts.add(AccountEntity.builder()
+                .username("admin")
+                .passwordHash(encoder.encode("123456"))
+                .gender(1)
+                .accountRole(AccountRole.ADMIN)
+                .enabled(true)
+                .email("admin@gmail.com")
+                .displayName("Admin")
+                .build());
+        List<String> userNames = List.of(
+                "alex.miller",
+                "sophie.williams",
+                "jake.turner",
+                "mia.johnson",
+                "lucas.thompson",
+                "emma.brown",
+                "noah.davis",
+                "olivia.moore",
+                "liam.taylor",
+                "ava.anderson"
+        );
+        for (int i = 1; i <= 10; i++) {
+            accounts.add(AccountEntity.builder()
+                    .username(userNames.get(i - 1))
+                    .passwordHash(encoder.encode("123456"))
+                    .accountRole(AccountRole.USER)
+                    .gender(i % 2)
+                    .enabled(true)
+                    .email(userNames.get(i - 1).replace(".", "") + "@gmail.com")
+                    .displayName(userNames.get(i-1).replace(".", " "))
+                    .build());
         }
+
+        accountRepository.saveAll(accounts);
+        log.info("Accounts created successfully");
     }
 }
+
